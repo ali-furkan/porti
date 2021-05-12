8b7a48be2a3e43694c7adb24d30530b915e42a73
ali-furkan <a.furkan.2003@gmail.com>
2021-05-12 11:26:32 +0300

    :truck: : rename app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
+import Toast from "react-native-toast-message";
 
 import client from "@/lib/upstash-client";
-import Navigator from "@/Navigator"
+import Navigator from "@/navigator";
+import {StoreProvider} from "@/contexts/store/store.context"
+
 
 const App = () => {
 	return (
 		<ApolloProvider client={client}>
-			<Navigator />
+			<Toast ref={(ref) => Toast.setRef(ref)} />
+			<StoreProvider>
+				<Navigator />
+			</StoreProvider>
 		</ApolloProvider>
 	);
 };
