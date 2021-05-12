19eebad34c6a545ef02f46a6ec6e6c8fb99f12bb
ali-furkan <a.furkan.2003@gmail.com>
2021-05-12 11:20:36 +0300

    :sparkles: create store context

---
+++ src/contexts/store/store.context.js
@@ -0,0 +1,82 @@
+import React, { useReducer, useEffect, useContext, useMemo } from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_BOOKMARKS } from "@/lib/bookmarks";
+import { getPostList, usePost } from "@/lib/posts";
+
+import storeReducer, { actions } from "./store.reducer";
+
+export const StoreContext = React.createContext();
+
+function Initializor() {
+	const bookmark = useQuery(GET_BOOKMARKS);
+	const post = usePost(36, getPostList);
+	const [, store] = useStore();
+
+	useEffect(() => {
+		store.setData("bookmarks", {
+			loading: bookmark.loading,
+			error: bookmark.error,
+			data: bookmark.data
+				? bookmark.data.redisHGetAll.map(({ field, value }) => ({
+						field,
+						id: field,
+						...JSON.parse(value),
+				  }))
+				: null,
+		});
+		store.setData("posts", post);
+	}, [bookmark, post]);
+
+	return null;
+}
+const MemoInitializor = React.memo(Initializor);
+
+export function StoreProvider({ children }) {
+	const [state, dispatch] = useReducer(storeReducer, { });
+
+	const storeContext = useMemo(
+		() => [
+			state,
+			{
+				updateItem: (field, itemField, value) =>
+					dispatch({
+						type: actions.UPDATE_ITEM,
+						field,
+						item: itemField,
+						data: value,
+					}),
+				createItem: (field, data) =>
+					dispatch({
+						type: actions.CREATE_ITEM,
+						field,
+						data,
+					}),
+				deleteItem: (field, itemField) =>
+					dispatch({
+						type: actions.DELETE_ITEM,
+						field,
+						item: itemField,
+					}),
+				setData: (field, value) =>
+					dispatch({ type: actions.SET_DATA, field, data: value }),
+				delete: (field) => dispatch({ type: actions.DELETE_ITEM, field }),
+				reset: () => dispatch({ type: actions.RESET }),
+			},
+		],
+		[state],
+	);
+
+	return (
+		<StoreContext.Provider value={storeContext}>
+			<MemoInitializor />
+			{children}
+		</StoreContext.Provider>
+	);
+}
+
+export const useStore = () => {
+	const [state, dispatch] = useContext(StoreContext);
+
+	return [state, dispatch];
+};
---
+++ src/contexts/store/store.reducer.js
@@ -0,0 +1,45 @@
+export const actions = {
+	UPDATE_ITEM: Symbol("UPDATE_ITEM"),
+	DELETE_ITEM: Symbol("DELETE_ITEM"),
+	CREATE_ITEM: Symbol("CREATE_ITEM"),
+	SET_DATA: Symbol("SET_DATA"),
+	RESET: Symbol("RESET"),
+};
+
+export default (prevState, a) => {
+	switch (a.type) {
+		case actions.UPDATE_ITEM:
+			return {
+				...prevState,
+				[a.field]:   {
+					...prevState[a.field],
+					data: prevState[a.field].data.map((i) => (a.item === i.id ? a.data : i)),
+				}
+			};
+		case actions.CREATE_ITEM: return {
+			...prevState,
+			[a.field]:   {
+				...prevState[a.field],
+				data: [
+					...prevState[a.field].data,
+					a.data
+				]
+			}
+		};
+		case actions.DELETE_ITEM:
+			return {
+				...prevState,
+				[a.field]: {
+					...prevState[a.field],
+					data: prevState[a.field].data.filter((i) => (a.item === i.id ? false : true)),
+				}
+			};
+		case actions.SET_DATA:
+			return {
+				...prevState,
+				[a.field]: a.data,
+			};
+		case actions.RESET:
+			return {};
+	}
+};
